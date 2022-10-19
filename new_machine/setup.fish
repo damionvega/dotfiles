754e4a8028a43f40a367a4c4aa2d6fedffcfe7b9
Damion Vega <hi@djv.io>
2022-10-18 23:27:47 -0700

    Add postgres path

--- new_machine/setup.fish
+++ new_machine/setup.fish
@@ -1,7 +1,5 @@
 #!/opt/homebrew/bin/fish
 
-log 'setup.fish: Start'
-
 function log
     echo
     echo "💬 $argv..."
@@ -11,9 +9,8 @@ set FISH_PATH (which fish)
 set BREW_PATH /opt/homebrew/bin
 
 log 'Verifying Homebrew binaries available in $PATH'
-set BREW_GREP (echo $PATH | grep $BREW_PATH)
 
-if not string match -e $BREW_PATH $BREW_GREP
+if not string match -e $BREW_PATH (echo $PATH | grep $BREW_PATH)
     log "Adding $BREW_PATH to PATH"
     set -U fish_user_paths $BREW_PATH $fish_user_paths
     fish_add_path $BREW_PATH
@@ -23,9 +20,8 @@ else
 end
 
 log 'Verifying Fish path exists in /etc/shells'
-set FISH_GREP (cat /etc/shells | grep $FISH_PATH)
 
-if not string match -e $FISH_PATH $FISH_GREP
+if not string match -e $FISH_PATH (cat /etc/shells | grep $FISH_PATH)
     log "Adding $FISH_PATH to /etc/shells"
     # echo $FISH_PATH | sudo tee -a /etc/shells
     sudo sh -c "echo $FISH_PATH >> /etc/shells"
@@ -45,11 +41,13 @@ fisher install jethrokuan/z
 
 log 'Installing latest Node.js LTS with jorgebucaran/nvm.fish'
 nvm install lts
-nvm use lts
+
+log 'Creating postgres user'
+fish_add_path /opt/homebrew/opt/postgresql@12/bin
+createuser -s postgres
+brew services restart postgresql
 
 log 'Updating Fish completions'
 fish_update_completions
-
-log 'setup.fish: Done'
 
 exit
